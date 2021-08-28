84b54a8949e16973c16d9f1d9584c5e84bbf2b5c
Malcolm Belfrom <mibelfrom@gmail.com>
2021-08-27 19:35:43 -0700

    pass gen debugging

--- script.js
+++ script.js
@@ -1,23 +1,78 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+generateBtn.addEventListener("click", writePassword);
+
+// Why do we have these strings here? 
+// we use these strings to pull data from for the passowrd generator. 
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var numeric = "0123456789";
+var special = "!@#$%^&*()_+";
+function getRandom(str) {
+  // What is going on here?
+  // it is going to generate a random number 
+  var randomIndex = Math.floor(Math.random() * str.length);
+  // and here?
+  // This is going to return a random number but with in the string length that we created above. 
+  return str[randomIndex];
+}
+// Write password to the #password input 
 function writePassword() {
+
+
+  // What is the difference between return and console log?
+  //
+  // What happens if we console log our password instead of returning it?
+  //
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
-
 function generatePassword() {
   // Ask the user for their password criteria
-var pssWord = prompt("Length of password should be between 8 and 128 characters", "");
-var psswrd2 = prompt("Your password can include lowercase, uppercase, numeric, and/or special characters", "");
+  // 8 characters and no more than 128 characters
+  // Prompt the user for the password length
+  var length = prompt("How long would you like your password to be?");
+
+  // If the length is less than 8 or more than 128
+  if (length < 8 || length > 128) {
+    // alert the user that they messed up
+    alert("Your password must be between 8 and 128");
+    // exit the function early, (use the return keyword)
+    return "";
+  }
+  // What are we doing here?
+  var useLowercase = confirm("Would you like to use lower case characters?");
+  var useUppercase = confirm("Would you like to use upper case characters?");
+  var useNumeric = confirm("Would you like to use numeric characters?");
+  var useSpecial = confirm("Would you like to use special characters?");
+  // Why is this if statement necessary?
+  
+  //User must supply at least one character type to generate a password
+
+  if (!useLowercase && !useUppercase && !useNumeric && !useSpecial) {
+    alert("We need at least one option to be chosen.");
+    return "";
+  }
   // generate the password based off the criteria
-
-  // `return` that password
-  return "Avery"
-}
-
+  var pass = "";
+  
+  for (let i = 0; i < length; i++)  {
+    if (useLowercase) {
+      pass += getRandom(lowercase);
+    }
+    if (useUppercase) {
+      pass += getRandom(uppercase);
+    }
+    if (useNumeric) {
+      pass += getRandom(numeric);
+    }
+    if (useSpecial) {
+      pass += getRandom(special);
+    }
+  }
+  return pass.substr(0, length);
+} 
+// `return` that password
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
